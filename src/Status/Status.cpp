3f8dff590b9c8455bc98806c28064a913eb12bc3
ac973k <daniil@terentyeff.ru>
2024-12-26 19:11:26 +0300

    change methods Status on VK API

--- src/Status/Status.cpp
+++ src/Status/Status.cpp
@@ -12,36 +12,27 @@ namespace VK {
 
         }
 
-        void Status::getUser(const QString &access_token, const QString &user_id)
+        void Status::getUser(const QString &access_token, const QString &scopes)
         {
-            QUrl link(QString("https://api.vk.com/method/status.get?access_token=%1&user_id=%2&v=5.199").arg(access_token).arg(user_id));
+            QUrl link(QString("https://api.vk.com/method/status.get?access_token=%1&%2&v=5.199").arg(access_token).arg(scopes));
             qDebug() << "full url: " << link;
             QNetworkRequest request(link);
             QNetworkReply* reply = networkManager.get(request);
             connect(reply, &QNetworkReply::finished, this, &Status::Finished);
         }
 
-        void Status::getGroup(const QString &access_token, const QString &group_id)
+        void Status::getGroup(const QString &access_token, const QString &scopes)
         {
-            QUrl link(QString("https://api.vk.com/method/status.get?access_token=%1&group_id=%2&v=5.199").arg(access_token).arg(group_id));
+            QUrl link(QString("https://api.vk.com/method/status.get?access_token=%1&%2&v=5.199").arg(access_token).arg(scopes));
             qDebug() << "full url: " << link;
             QNetworkRequest request(link);
             QNetworkReply* reply = networkManager.get(request);
             connect(reply, &QNetworkReply::finished, this, &Status::Finished);
         }
 
-        void Status::set(const QString &access_token, const QString &text, const QString &group_id)
+        void Status::set(const QString &access_token, const QString &scopes)
         {
-            QUrl link(QString("https://api.vk.com/method/status.set?access_token=%1&text=%2&group_id=%3&v=5.199").arg(access_token).arg(text).arg(group_id));
-            qDebug() << "full url: " << link;
-            QNetworkRequest request(link);
-            QNetworkReply* reply = networkManager.get(request);
-            connect(reply, &QNetworkReply::finished, this, &Status::Finished);
-        }
-
-        void Status::set(const QString &access_token, const QString &text)
-        {
-            QUrl link(QString("https://api.vk.com/method/status.set?access_token=%1&text=%2&v=5.199").arg(access_token).arg(text));
+            QUrl link(QString("https://api.vk.com/method/status.set?access_token=%1&%2&v=5.199").arg(access_token).arg(scopes));
             qDebug() << "full url: " << link;
             QNetworkRequest request(link);
             QNetworkReply* reply = networkManager.get(request);
--- src/Status/Status.h
+++ src/Status/Status.h
@@ -17,10 +17,9 @@ namespace VK {
                 explicit Status(QObject *parent = nullptr);
                 ~Status();
 
-                void getUser(const QString &access_token, const QString &user_id);
-                void getGroup(const QString &access_token, const QString &group_id);
-                void set(const QString &access_token, const QString &text, const QString &group_id);
-                void set(const QString &access_token, const QString &text);
+                void getUser(const QString &access_token, const QString &scopes);
+                void getGroup(const QString &access_token, const QString &scopes);
+                void set(const QString &access_token, const QString &scopes);
 
             private:
                 QNetworkAccessManager networkManager;
